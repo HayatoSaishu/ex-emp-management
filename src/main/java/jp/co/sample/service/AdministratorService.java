1b4a534543faffcf92b34dfb03399cc603b65fe7
saishuhayato <hayato.saishu@rakus.partners.co.jp>
2021-08-05 13:46:19 +0900

    AdministratorServiceにjavadoc追加

--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -6,6 +6,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import jp.co.sample.repository.AdministratorRepository;
 
+/**
+ * 管理者情報の業務処理を行うサービス.
+ * 
+ * @author hayato.saishu
+ *
+ */
 @Service
 @Transactional
 public class AdministratorService {
